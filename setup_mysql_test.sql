ce88b2d534eec3d3eb45004cd8737a3f67e8a14a
Kgaugelo-Shai <144380460+Kgaugelo-Shai@users.noreply.github.com>
2024-04-18 21:50:53 +0200

    Task 4: setup_mysql_test

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,7 +1,11 @@
 -- Creates a MySQL server with:
 -- Create a DATABASE hbnb_test_db
+CREATE DATABASE IF NOT EXISTS hbnb_test_db;
 -- Create a new USER hbnb_test with password hbnb_test_pwd in localhost
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
 -- Grant all privileges for hbnb_test on hbnb_test_db
+GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
+FLUSH PRIVILEGES;
 -- Grant select privileges for hbnb_test on performance_schema
-
-
+GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
+FLUSH PRIVILEGES;
