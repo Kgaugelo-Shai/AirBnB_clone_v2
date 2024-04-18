f4c4a40c3d5ce7377af3462a288da4d2a28c8a04
lulokent <cwengalulonkem@gmail.com>
2024-04-18 01:25:05 -0700

    incomplete task 4

---
+++ setup_mysql_test.sql
@@ -0,0 +1,7 @@
+-- Creates a MySQL server with:
+-- Create a DATABASE hbnb_test_db
+-- Create a new USER hbnb_test with password hbnb_test_pwd in localhost
+-- Grant all privileges for hbnb_test on hbnb_test_db
+-- Grant select privileges for hbnb_test on performance_schema
+
+
