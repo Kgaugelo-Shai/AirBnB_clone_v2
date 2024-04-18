959a486cba981b9818ba51fbef4529d6b0941915
Kgaugelo-Shai <144380460+Kgaugelo-Shai@users.noreply.github.com>
2024-04-18 22:57:51 +0200

    file storage: delete and all methods

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -8,9 +8,20 @@ class FileStorage:
     __file_path = 'file.json'
     __objects = {}
 
-    def all(self):
+    def all(self, cls=None):
         """Returns a dictionary of models currently in storage"""
-        return FileStorage.__objects
+        if cls is None:
+            return FileStorage.__objects
+        obj_dict = {}
+        # get the class name
+        name = cls.__name__
+        # iterate through the key values of my __object dict
+        for k in FileStorage.__objects.keys():
+            # I extract the class name from the . id
+            if k.split('.')[0] == name:
+                # I populate my dict with the objects of the same kind
+                obj_dict[k] = FileStorage.__objects[k]
+        return obj_dict
 
     def new(self, obj):
         """Adds new object to storage dictionary"""
@@ -48,3 +59,18 @@ class FileStorage:
                         self.all()[key] = classes[val['__class__']](**val)
         except FileNotFoundError:
             pass
+
+
+    def delete(self, obj=None):
+        """ Function that deletes obj in __object if it is inside
+            - if obj is not inside, it does nothing
+        """
+        if obj is None:
+            return
+        new_obj = obj.to_dict()
+        class_name = new_obj['__class__']
+        obj_id = obj.id
+        key_obj = class_name + '.' + obj_id
+        if key_obj in self.__object.keys():
+            print(key_obj)
+            del self.__objects[key_obj]
