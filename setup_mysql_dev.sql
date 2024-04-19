a5d9a1821883b89aa0cc90fcf403a606a9b4ee9a
lulokent <cwengalulonkem@gmail.com>
2024-04-19 00:10:35 -0700

    updated task 3

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,17 +1,15 @@
--- Prepares a MySQL server with:
--- Database hbnb_dev_db.
--- A new User hbnb_dev with password hbnb_dev_pwd in localhost.
--- Grants all privileges for hbnb_ev on hbnb_dev_db.
--- Grants SELECT privilege for Hbnb_dev on performance.
-
+-- Creates a MySQL server with:
 -- Creates the database if it doesn't exist.
-CREATE DATABASE IF NOT EXITS hbnb_dev_db;
+CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 
 -- Create the user if it doesn't exist
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 
--- Grant Privileges on hbnb_devdb
+-- Grant Privileges on hbnb_dev_db
 GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
+
+-- Select the database
+USE hbnb_dev_db;
 
 -- Grant SELECT Privilege on performance schema
 GRANT SELECT ON performance_schema * TO 'hbnb_dev'@'localhost';
