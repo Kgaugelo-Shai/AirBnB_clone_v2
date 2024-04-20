00e637b7a50f6942879a1139280ec3f42fa9e469
Kgaugelo-Shai <144380460+Kgaugelo-Shai@users.noreply.github.com>
2024-04-20 16:52:45 +0200

    city and state - task 6

--- models/city.py
+++ models/city.py
@@ -1,9 +1,21 @@
 #!/usr/bin/python3
 """ City Module for HBNB project """
 from models.base_model import BaseModel
+from models import hbnb_storage_type
+from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import relationship
 
 
 class City(BaseModel):
     """ The city class, contains state ID and name """
-    state_id = ""
-    name = ""
+    __table__ = 'cities'
+
+    if hbnb_storage_type == 'db':
+        name = Column(String(128),
+                      nullable=False)
+        state_id = Column(String(60),
+                          ForeignKey('states.id'),
+                          nullable=False)
+    else:
+        name = ''
+        state_id = ''
--- models/state.py
+++ models/state.py
@@ -1,8 +1,34 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
 from models.base_model import BaseModel
+from models.city import City
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
+from models import hbnb_stoarage_type
 
 
 class State(BaseModel):
     """ State class """
-    name = ""
+    __tablename__ = 'states'
+
+    if hbnb_torage_type == 'db':
+        name = Column(String(128),
+                      nullable=False)
+        cities = relationship('City',
+                              backref='state',
+                              cascade='all, delete, delete-orphan')
+    else:
+        name = ""
+
+    @property
+    def cities(self):
+        """returns the list of City instances with state_id equals the current
+           State.id =>
+           It will be a FileStorage relationship between State and City
+        """
+        cities_related = []
+        cities = storage.all(City)
+        for city in cities.values():
+            if city.state_id == self.id:
+                cities_related.append(city)
+        return cities_related
