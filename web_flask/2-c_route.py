ac6e408e61e1df7553683653e7346cbc0e56eaee
Kgaugelo-Shai <144380460+Kgaugelo-Shai@users.noreply.github.com>
2024-05-22 15:03:00 +0200

    task 2

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+""" Starts a Flask web application """
+from flask import Flask
+web_app = Flask(__name__)
+
+
+@web_app.route('/', strict_slashes=False)
+def flask_greeting():
+    """ Display greeting """
+    return "Hello HBNB!"
+
+
+@web_app.route('/hbnb', strict_slashes=False)
+def flask_greeting2():
+    """ Display greeting"""
+    return "HBNB"
+
+@web_app.route("/c/<text>", strict_slashes=False)
+def c_text(text):
+    """ formated text returned as a string"""
+    return 'C ' + text.replace('_', ' ')
+
+
+if __name__ == "__main__":
+    web_app.run('0.0.0.0')
