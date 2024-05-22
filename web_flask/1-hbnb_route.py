1202e96c77a2b4abb861e7a83d9953d24efd4170
Kgaugelo-Shai <144380460+Kgaugelo-Shai@users.noreply.github.com>
2024-05-22 14:53:35 +0200

    task 1

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+""" Starts a Flask web application """
+from flask import Flask
+web_app = Flask(__name__)
+
+
+@web_app.route('/', strict_slashes=False)
+def flask_greeting():
+    """ Display greeting """
+    return "Hello HBNB!"
+
+@web_app.route('/hbnb', strict_slashes=False)
+def flask_greeting2():
+    """ Display greeting"""
+    return "HBNB"
+
+
+if __name__ == "__main__":
+    web_app.run('0.0.0.0')
