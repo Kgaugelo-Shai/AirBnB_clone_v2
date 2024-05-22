aab473b9fa5588cd89553922dec76109a81cd048
Kgaugelo-Shai <144380460+Kgaugelo-Shai@users.noreply.github.com>
2024-05-22 22:19:22 +0200

    task 7 fix

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,16 +1,17 @@
 #!/usr/bin/python3
 """ Starts a web flask app """
 from flask import Flask, render_template
+from models.state import State
 from models import storage
-from models.state import state
 web_app = Flask(__name__)
 
 
-@web_app.route('/states_list', strict_slashes=False)
+@web_app.route("/states_list", strict_slashes=False)
 def states_list():
     """ loads template states"""
-    state_list =  storage.all("State")
-    return render_template("7-states_list.html", state_list=state_list)
+    state_list = storage.all(State)
+    print(state_list)
+    return render_template("7-states_list.html", state=state_list)
 
 
 @web_app.teardown_appcontext
@@ -20,4 +21,4 @@ def teardown(arg=None):
 
 
 if __name__ == "__main__":
-    web_app.run("0.0.0.0")
+    web_app.run(host="0.0.0.0")
